71852e9ed79eacf28d201738f4f113d61cfe9174
sergiosrdev <advsergiorodrigues@gmail.com>
2026-03-10 14:55:53 -0300

    Configure Vite to run on port 5500 locally

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  server: {
+    host: "127.0.0.1",
+    port: 5500,
+    strictPort: true,
+  },
+  preview: {
+    host: "127.0.0.1",
+    port: 5501,
+    strictPort: true,
+  },
 });
